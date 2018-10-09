2817c44789c2b30e06f0792e8d1246e597ae09ce
Gustavo Felipe <gustavofuracao06@hotmail.com>
2018-10-09 18:09:29 -0300

    Commit iNICIAL

---
+++ Cinefolis/Cinefolis/Cinefolis/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Cinefolis.Models;
+
+namespace Cinefolis.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
