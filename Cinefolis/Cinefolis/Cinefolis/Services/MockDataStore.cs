61efc9ff31a5ea6f51ef171bb41637c78cf68a75
Wagner L. Pereira <wagnerluis1989@gmail.com>
2018-10-26 20:27:14 -0300

    Ajuste para funcionar

--- Cinefolis/Cinefolis/Cinefolis/Services/MockDataStore.cs
+++ Cinefolis/Cinefolis/Cinefolis/Services/MockDataStore.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Android.Content.Res;
+//using Android.Content.Res;
 using Cinefolis.Models;
 
 namespace Cinefolis.Services
